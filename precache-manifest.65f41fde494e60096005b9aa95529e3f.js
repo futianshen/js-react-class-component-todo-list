6ed66a63e87ce8e766773f769c284fa6bb538078
ftshen <futianshen@MacBook-Pro.local>
2019-01-31 22:10:24 +0800

    Updates

---
+++ precache-manifest.65f41fde494e60096005b9aa95529e3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "cb6e9ab893872652ff69",
+    "url": "/react-todo/static/js/main.cb6e9ab8.chunk.js"
+  },
+  {
+    "revision": "d44c0fc78c8a8aa2f697",
+    "url": "/react-todo/static/js/1.d44c0fc7.chunk.js"
+  },
+  {
+    "revision": "cb6e9ab893872652ff69",
+    "url": "/react-todo/static/css/main.be633ea3.chunk.css"
+  },
+  {
+    "revision": "096e1b0d8a18855620c3660e8145d253",
+    "url": "/react-todo/index.html"
+  }
+];
